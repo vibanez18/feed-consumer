4714bd6b1d8998d9ee395a5c5c431eb82056f251
Victor Ibañez <victor.ibanez@flowable.com>
2021-04-20 17:53:07 +0200

    First commit - Init project

--- server/src/main/java/com/sparta/controller/MainController.java
+++ server/src/main/java/com/sparta/controller/MainController.java
@@ -1,20 +1,20 @@
 package com.sparta.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 public class MainController {
 
   @PostMapping("/load/{provider}")
   public int load(@PathVariable("provider") String provider, @RequestBody byte[] content) throws IOException {
+    return 0;
   }
 
   @GetMapping("/data/{provider}/total")
   public int total(@PathVariable("provider") String provider) {
+    return 0;
   }
 
 }
